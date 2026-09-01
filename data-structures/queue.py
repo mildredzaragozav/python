e678d1bdd92647972fe2689e09433249e54415f9
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-08-31 21:45:28 -0600

    Adding an implementation of 'Queue' data structure

---
+++ data-structures/queue.py
@@ -0,0 +1,83 @@
+"""
+    A Queue is a linear data structure that operates according to the FIFO principle.
+    This is an implementation of a Queue using a linked list. 
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.front = new_node
+        self.rear = new_node
+        self.length = 1
+
+    def enqueue(self, value):
+        temp = Node(value)
+        if self.is_empty():
+            self.front = temp
+        else:
+            self.rear.next = temp
+
+        self.rear = temp
+        self.length += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+
+        value = self.front.value
+
+        if self.length == 1:    
+            self.front = None
+            self.rear = None
+        else:
+            self.front = self.front.next
+
+        self.length -= 1
+        return value
+    
+    def peek(self):
+        if self.is_empty():
+            return None
+
+        return self.front.value
+    
+    def is_empty(self):
+        return self.length == 0
+
+    def print(self):
+            temp = self.front
+            while temp is not None:
+                print(temp.value, end=" -> ")
+                temp = temp.next
+            print("None") 
+
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+my_queue.enqueue(3)
+my_queue.print()
+print("Peek: ", my_queue.peek())
+print("Length: ", my_queue.length)
+print("isEmpty ? : ", my_queue.is_empty(), "\n")
+
+my_queue.dequeue()
+my_queue.dequeue()
+my_queue.dequeue()
+my_queue.print()
+print("Peek: ", my_queue.peek())
+print("Length: ", my_queue.length())
+print("isEmpty ? : ", my_queue.is_empty(), "\n")
+
+my_queue.dequeue()
+my_queue.enqueue(4)
+my_queue.enqueue(5)
+my_queue.print()
+print("Peek: ", my_queue.peek())
+print("Length: ", my_queue.length())
+print("isEmpty ? : ", my_queue.is_empty(), "\n")
+
